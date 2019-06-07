d1c9c5ded8406d9b26208575bb0a6cfd9887bceb
phatu <phatu.makushu@umuzi.org>
2019-06-07 14:58:26 +0200

    first commit

---
+++ spec/stringSpec.js
@@ -0,0 +1,35 @@
+
+describe("addNegative", function(){
+
+    it ("should add two numbers given as strings", function(){
+        expect(addNegative("3,4")).toBe(7);
+    });
+
+
+    it ("should add more than numbers given as strings", function(){
+        expect(addNegative("1,2,3,4")).toBe(10);
+    });
+
+
+    it ("should add numbers with new line in the string", function(){
+        expect(addNegative("1\n2,3")).toBe(6);
+    });
+
+    it ("should add numbers with different delimeters in the string", function(){
+        expect(addNegative("//;\n1;2")).toBe(3);
+    });
+    
+
+    it ("should add return 0 if string is empty", function(){
+        expect(addNegative("")).toBe(0);
+    });
+
+    
+    it("should throw an exception if there are negative numbers", () => {
+        expect(() => { addNegative("-7,3") }).toThrowError();
+});
+
+
+});
+
+  
---
+++ src/string.js
@@ -0,0 +1,52 @@
+
+function addNegative(num){
+
+    //spliting the string
+num = num.split('');
+var neg = 0,
+    minus = '-',
+    sum = 0,
+    newNum = 0;
+
+
+    //looping through everything to get the negatives
+for(var a=0; a<num.length; a++){
+   
+    if(num[a] == minus){
+         neg += num[a+1];
+    }
+}
+
+    //if the string is empty, 0 will be returned
+if(num == ""){
+    return 0;
+
+    //if there is a negative, an exception will be thrown
+} else if (neg) {
+    neg = neg.split('');
+
+    for(var a=0; a<neg.length; a++){
+        neg[a] *= -1;
+    }
+    neg = neg.join('');
+    throw new Error("no negatives are allowed" + " " + neg); 
+
+    //if there are positives, sum will be returned
+} else {
+    for(var a=0; a<num.length; a++){
+    var divide = num[a] / 1;; 
+
+    if(Number(divide)){
+        newNum += num[a];
+    }
+}
+
+for(var a=0; a<newNum.length; a++){
+    sum += parseInt(newNum[a]);
+}
+    return sum; 
+}
+
+
+}
+
