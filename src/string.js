1c91d8b5db02f164b4b4cdb309373f348a117a96
makushu <phatumakushu@gmail.com>
2019-09-19 16:30:02 +0200

    Update string.js

--- src/string.js
+++ src/string.js
@@ -2,51 +2,48 @@
 function addNegative(num){
 
     //spliting the string
-num = num.split('');
-var neg = 0,
+    num = num.split('');
+    var neg = 0,
     minus = '-',
     sum = 0,
     newNum = 0;
 
 
     //looping through everything to get the negatives
-for(var a=0; a<num.length; a++){
-   
-    if(num[a] == minus){
-         neg += num[a+1];
+    for(var a=0; a<num.length; a++){
+        if(num[a] == minus){
+            neg += num[a+1];
+        }
     }
-}
 
     //if the string is empty, 0 will be returned
-if(num == ""){
-    return 0;
+    if(num == ""){
+        return 0;
 
     //if there is a negative, an exception will be thrown
-} else if (neg) {
-    neg = neg.split('');
+    } else if (neg) {
+        neg = neg.split('');
 
-    for(var a=0; a<neg.length; a++){
-        neg[a] *= -1;
+        for(var a=0; a<neg.length; a++){
+            neg[a] *= -1;
     }
-    neg = neg.join('');
-    throw new Error("no negatives are allowed" + " " + neg); 
+        neg = neg.join('');
+        throw new Error("no negatives are allowed" + " " + neg); 
 
     //if there are positives, sum will be returned
-} else {
+    } else {
     for(var a=0; a<num.length; a++){
-    var divide = num[a] / 1;; 
+        var divide = num[a] / 1;; 
 
-    if(Number(divide)){
-        newNum += num[a];
+        if(Number(divide)){
+            newNum += num[a];
+        }
     }
-}
 
-for(var a=0; a<newNum.length; a++){
-    sum += parseInt(newNum[a]);
-}
-    return sum; 
-}
-
-
+    for(var a=0; a<newNum.length; a++){
+        sum += parseInt(newNum[a]);
+    }
+        return sum; 
+    }
 }
 
